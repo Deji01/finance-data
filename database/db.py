c128cea0a9565224bf42a3a8f75673db73f75aaf
Ayodeji Yekeen <ayodejiyou@gmail.com>
2024-02-20 12:18:51 +0000

    refactor db

--- database/db.py
+++ database/db.py
@@ -1,15 +1,15 @@
 from glob import glob
 import os
 import pandas as pd
-from model import CsvData
+from model import CsvData, Config
 from sqlmodel import SQLModel, Session, create_engine
 
 import logging
 from logging.handlers import RotatingFileHandler
 
 # Create logs directory if it does not exist
-if not os.path.exists('logs'):
-    os.makedirs('logs')
+if not os.path.exists("logs"):
+    os.makedirs("logs")
 
 # Set up logging with both stream handler and file handler at INFO level
 logger = logging.getLogger(__name__)
@@ -17,15 +17,20 @@ logger.setLevel(logging.INFO)
 
 # Stream handler for console output
 stream_handler = logging.StreamHandler()
-stream_handler_formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+stream_handler_formatter = logging.Formatter(
+    "%(asctime)s - %(levelname)s - %(message)s"
+)
 stream_handler.setFormatter(stream_handler_formatter)
 logger.addHandler(stream_handler)
 
 # File handler for output to a log file with rotation
-log_file_handler = RotatingFileHandler('logs/main.log', maxBytes=1048576, backupCount=5)
-file_handler_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+log_file_handler = RotatingFileHandler("logs/main.log", maxBytes=1048576, backupCount=5)
+file_handler_formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
 log_file_handler.setFormatter(file_handler_formatter)
 logger.addHandler(log_file_handler)
+
 
 def load_data():
     """
@@ -51,9 +56,7 @@ if __name__ == "__main__":
     logger.info("CSV data loaded successfully.")
 
     # create an engine to connect to the PostgreSQL database
-    engine = create_engine(
-        "postgresql://your_username:your_password@your_host:your_port/your_dbname"
-    )
+    engine = create_engine(Config().POSTGRES_URL)
 
     logger.info("Connected to the PostgreSQL database.")
 
@@ -62,14 +65,7 @@ if __name__ == "__main__":
     with Session(engine) as session:
         # load data from pandas dataframe into the database
         for index, row in csv_data.iterrows():
-            session.add(
-                CsvData(
-                    source=row["source"],
-                    url=row["url"],
-                    content=row["content"],
-                    article=row["article"],
-                )
-            )
+            session.add(CsvData(**row.to_dict()))
             if index % 20 == 0:
                 logger.info(f"Processed {index} rows.")
         logger.info("Data loaded successfully.")
--- database/model.py
+++ database/model.py
@@ -1,11 +1,17 @@
 # create an SQLModel class that models the CSV data
 from sqlmodel import SQLModel, Field
 from typing import Optional
+from pydantic_settings import Settings
+
+
+class Config(Settings):
+    POSTGRES_URL: str
+    env_file = ".env"
+
 
 class CsvData(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    source: str = Field(nullable=False)
+    source: Optional[str] = None
     url: Optional[str] = None
-    content: str
-    article: str
-
+    content: Optional[str] = None
+    article: Optional[str] = None

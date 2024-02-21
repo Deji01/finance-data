e9ffb919f7bd3e1418972131bcea038da8c66c91
Ayodeji Yekeen <ayodejiyou@gmail.com>
2024-02-21 02:52:03 +0000

    update env

--- stock-titan/config.py
+++ stock-titan/config.py
@@ -2,11 +2,6 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    DB_HOST: str
-    DB_NAME: str
-    DB_USER: str
-    DB_PASSWORD: str
-    DB_PORT: str
     DB_URL: str
 
     class Config:
